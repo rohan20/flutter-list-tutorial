742f513ad9b1e8beb19e14dd7136fce9a3f0f227
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-23 02:51:35 +0530

    added EnglishWords dependency and displayed a random word

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(HomePage());
@@ -7,12 +8,18 @@ void main() {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final randomWord = WordPair.random().asPascalCase;
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Lazy List"),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Lazy List"),
+      ),
+      body: Center(
+        child: Text(
+          randomWord,
         ),
-      )
-    );
+      ),
+    ));
   }
 }
