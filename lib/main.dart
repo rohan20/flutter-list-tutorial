5fdfb49d87fee257d06842d1485b7e629faa7195
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-23 02:47:33 +0530

    setup basic appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
-void main() {
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Lazy List"),
+        ),
+      )
+    );
+  }
 }
